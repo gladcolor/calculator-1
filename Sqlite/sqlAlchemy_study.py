59c6079377abc1594c5019e974802b2a882eea59
HuanNing <gladcolor@gmail.com>
2019-12-07 08:26:14 -0500

    Task: Inserting Data.

--- Sqlite/sqlAlchemy_study.py
+++ Sqlite/sqlAlchemy_study.py
@@ -1,7 +1,7 @@
 import sqlite3
 import sqlalchemy
 from sqlalchemy import ARRAY
-
+from sqlalchemy import *
 from sqlalchemy import create_engine, MetaData, Table, Integer, String, \
     Column, DateTime, ForeignKey, Numeric, CheckConstraint
 
@@ -53,7 +53,6 @@ from datetime import datetime
 
 Base = declarative_base()
 
-
 class Customer(Base):
     __tablename__ = 'customers'
     id = Column(Integer(), primary_key=True)
@@ -61,30 +60,32 @@ class Customer(Base):
     last_name = Column(String(100), nullable=False)
     username = Column(String(50), nullable=False)
     email = Column(String(200), nullable=False)
+    address = Column(String(200), nullable=False)
+    town = Column(String(200), nullable=False)
     created_on = Column(DateTime(), default=datetime.now)
     updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
-    orders = relationship("Order", backref='customer')
-
+    orders = relationship("Order", backref='customers')
 
 class Item(Base):
     __tablename__ = 'items'
     id = Column(Integer(), primary_key=True)
     name = Column(String(200), nullable=False)
-    cost_price = Column(Numeric(10, 2), nullable=False)
-    selling_price = Column(Numeric(10, 2), nullable=False)
-
-
+    cost_price =  Column(Numeric(10, 2), nullable=False)
+    selling_price = Column(Numeric(10, 2),  nullable=False)
 #     orders = relationship("Order", backref='customer')
 
+    quantity = Column(String(200), nullable=False)
 
+#
 class Order(Base):
     __tablename__ = 'orders'
     id = Column(Integer(), primary_key=True)
     customer_id = Column(Integer(), ForeignKey('customers.id'))
     date_placed = Column(DateTime(), default=datetime.now)
-    line_items = relationship("OrderLine", secondary="order_lines", backref='order')
-
-
+    line_items = relationship("OrderLine")#, secondary="order_lines")#, backref='orders')
+    customer  = relationship("Customer")
+    order_lines = relationship("OrderLine")
+#
 class OrderLine(Base):
     __tablename__ = 'order_lines'
     id = Column(Integer(), primary_key=True)
@@ -92,35 +93,110 @@ class OrderLine(Base):
     item_id = Column(Integer(), ForeignKey('items.id'))
     quantity = Column(SmallInteger())
     item = relationship("Item")
+    order = relationship("Order")
 
 
-Base.metadata.create_all(engine)
-#
-# clear_mappers()
-# mapper(Post, post)
+c1 = Customer(first_name='Toby',
+              last_name='Miller',
+              username='tmiller',
+              email='tmiller@example.com',
+              address='1662 Kinney Street',
+              town='Wolfden'
+              )
 
-#
-# for t in metadata.sorted_tables:
-#     print(t.name)  # print table name
+c2 = Customer(first_name='Scott',
+              last_name='Harvey',
+              username='scottharvey',
+              email='scottharvey@example.com',
+              address='424 Patterson Street',
+              town='Beckinsdale'
+              )
 
-#
-# c1 = Customer(first_name='Toby',
-#               last_name='Miller',
-#               username='tmiller',
-#               email='tmiller@example.com',
-#               address='1662 Kinney Street',
-#               town='Wolfden'
-#               )
-#
-# c2 = Customer(first_name='Scott',
-#               last_name='Harvey',
-#               username='scottharvey',
-#               email='scottharvey@example.com',
-#               address='424 Patterson Street',
-#               town='Beckinsdale'
-#               )
-# c1, c2
+print('c1.c1.first_name: ', str(c1.first_name))
 
+session.add(c1)
+session.add(c2)
+session.commit()
 
+c3 = Customer(
+    first_name="John",
+    last_name="Lara",
+    username="johnlara",
+    email="johnlara@mail.com",
+    address="3073 Derek Drive",
+    town="Norfolk"
+)
 
+c4 = Customer(
+    first_name="Sarah",
+    last_name="Tomlin",
+    username="sarahtomlin",
+    email="sarahtomlin@mail.com",
+    address="3572 Poplar Avenue",
+    town="Norfolk"
+)
+
+c5 = Customer(first_name='Toby',
+              last_name='Miller',
+              username='tmiller',
+              email='tmiller@example.com',
+              address='1662 Kinney Street',
+              town='Wolfden'
+              )
+
+c6 = Customer(first_name='Scott',
+              last_name='Harvey',
+              username='scottharvey',
+              email='scottharvey@example.com',
+              address='424 Patterson Street',
+              town='Beckinsdale'
+              )
+
+session.add_all([c3, c4, c5, c6])
+session.commit()
 # conn = sqlite3.connect('huan.db')
+
+i1 = Item(name='Chair', cost_price=9.21, selling_price=10.81, quantity=5)
+i2 = Item(name='Pen', cost_price=3.45, selling_price=4.51, quantity=3)
+i3 = Item(name='Headphone', cost_price=15.52, selling_price=16.81, quantity=50)
+i4 = Item(name='Travel Bag', cost_price=20.1, selling_price=24.21, quantity=50)
+i5 = Item(name='Keyboard', cost_price=20.1, selling_price=22.11, quantity=50)
+i6 = Item(name='Monitor', cost_price=200.14, selling_price=212.89, quantity=50)
+i7 = Item(name='Watch', cost_price=100.58, selling_price=104.41, quantity=50)
+i8 = Item(name='Water Bottle', cost_price=20.89, selling_price=25, quantity=50)
+
+session.add_all([i1, i2, i3, i4, i5, i6, i7, i8])
+session.commit()
+
+o1 = Order(customer=c1)
+o2 = Order(customer=c1)
+
+line_item1 = OrderLine(order=o1, item=i1, quantity=3)
+line_item2 = OrderLine(order=o1, item=i2, quantity=2)
+line_item3 = OrderLine(order=o2, item=i1, quantity=1)
+line_item3 = OrderLine(order=o2, item=i2, quantity=4)
+
+session.add_all([o1, o2])
+
+session.new
+session.commit()
+
+o3 = Order(customer=c1)
+orderline1 = OrderLine(item=i1, quantity=5)
+orderline2 = OrderLine(item=i2, quantity=10)
+
+o3.order_lines.append(orderline1)
+o3.order_lines.append(orderline2)
+
+session.add_all([o3])
+
+session.commit()
+
+print(c1.orders, o1.customer, c1.orders[0].order_lines, c1.orders[1].order_lines)
+for ol in c1.orders[0].order_lines:
+    print(ol.id, ol.item, ol.quantity)
+
+print('-------')
+
+for ol in c1.orders[1].order_lines:
+    print(ol.id, ol.item, ol.quantity)
